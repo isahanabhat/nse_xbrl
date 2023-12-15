68aaead57ca824dc6a0a59c773606420b964ed78
isahanabhat <isahanabhat@gmail.com>
2023-12-15 15:20:32 +0530

    Further changes

--- code/xbrl_parser.py
+++ code/xbrl_parser.py
@@ -2,15 +2,13 @@ import xml.etree.ElementTree as ET
 import re
 import pandas as pd
 
+
 class XBRLCorporateFilingParser:
-    def __init__(self, symbol, xbrl_str, exchange='nse', currency_unit=1E6, verbosity=1):
+    def __init__(self, symbol, xbrl_str, exchange='nse', currency_unit=1E6, verbosity=0):
         self.verbosity = verbosity
+        self.currency_unit = currency_unit
         tree = ET.ElementTree(ET.fromstring(xbrl_str))
         root = tree.getroot()
-
-        if self.verbosity >= 1:
-            print(root.tag)
-            print(root.attrib)
 
         pattern = r'[{}]'
         count = 0
@@ -33,44 +31,92 @@ class XBRLCorporateFilingParser:
         attributeFile = r"..\data\nse_xbrl_attribute_map.xlsx"
         self.attributeMap = pd.read_excel(attributeFile)
 
-        self.resultType = self.parsedDataFrame.loc[self.parsedDataFrame.Tags == 'ResultType']\
+        self.resultType = self.parsedDataFrame.loc[self.parsedDataFrame.Tags == 'ResultType'] \
             .Value.reset_index(drop=True)
         if len(self.resultType) == 0:
             self.resultType = 'default'
         else:
             self.resultType = self.resultType[0]
 
+        self.attribute_df = self.attributeMap.loc[
+            self.attributeMap['result_type'].str.contains(self.resultType, na=False)]
+
         if self.verbosity >= 1:
+            print("Root tag:", root.tag)
+            print("Root attribute:", root.attrib)
             print('Result type: ', self.resultType)
+            print("attribute_df: ")
+            print(self.attribute_df)
+
         if self.verbosity == 2:
             print(count)
             for i in rowlist:
                 print(i)
 
+    def calcXbrl(self, formula, context):
+        pattern = "[\[\]]"
+        formulaList = re.split(pattern, formula)
+        formulaString = ""
+        operands = self.attribute_df["attribute"].tolist()
+        for val in formulaList:
+            if val in operands:
+                actualAttribute = self.attribute_df.loc[(self.attribute_df.attribute == val) &
+                                                        (self.attribute_df['result_type'].str.contains(self.resultType,
+                                                                                                       na=False))] \
+                    .value_expr.reset_index(drop=True)[0]
+
+                numVal = self.parsedDataFrame.loc[(self.parsedDataFrame.Tags == actualAttribute) &
+                                                  (self.parsedDataFrame.contextRef == context)].Value.reset_index(
+                    drop=True)[0]
+                formulaString += numVal
+            else:
+                formulaString += val
+
+        # Last step to check for any dangerous command
+        assert 'os' not in formulaString and 'system' not in formulaString, \
+            'Aborting!!! Dangerous command: %s' % formulaString
+        return eval(formulaString)
 
     def get(self, attribute, context):
-        attributeName = self.attributeMap.loc[(self.attributeMap.attribute == attribute) &
-                                              (self.attributeMap.result_type == self.resultType)]\
-                        .value_expr.reset_index(drop=True)
-
-        data = self.parsedDataFrame.loc[(self.parsedDataFrame.Tags == attributeName[0]) &
-                                        (self.parsedDataFrame.contextRef == context)].reset_index(drop=True)
+        x = self.attribute_df.loc[(self.attribute_df.attribute == attribute) &
+                                  (self.attributeMap['result_type'].str.contains(self.resultType, na=False))] \
+            .reset_index(drop=True)
         if self.verbosity >= 1:
-            print('Attribute: ', attribute)
-            print('Actual attribute: ', attributeName)
-            print('Context: ', context)
-        return data.Value[0]
+            print("x dataframe:")
+            print(x)
+
+        if x.attribute_type[0] == 'raw_xbrl':
+            attributeName = self.attribute_df.loc[(self.attribute_df.attribute == attribute) &
+                                                  (self.attribute_df.result_type == self.resultType)] \
+                .value_expr.reset_index(drop=True)
+
+            data = self.parsedDataFrame.loc[(self.parsedDataFrame.Tags == attributeName[0]) &
+                                            (self.parsedDataFrame.contextRef == context)].reset_index(drop=True)
+            if self.verbosity >= 1:
+                print('Attribute: ', attribute)
+                print('Actual attribute: ', attributeName)
+                print('Context: ', context)
+                print(data)
+            resultValue = data.Value[0]
+        elif x.attribute_type[0] == 'calc_xbrl':
+            resultValue = self.calcXbrl(x.value_expr[0], context)
+
+
+        if x.value_type[0] == 'string':
+            return str(resultValue)
+        elif x.value_type[0] == 'float':
+            return (float(resultValue)) / self.currency_unit
 
 
 if __name__ == '__main__':
     file_bank = r'../data/BANKING_ICICIBANK.xml'
     file_default = r'../data/justSample.xml'
-    fileobj = open(file_default, 'r')
+    fileobj = open(file_bank, 'r')
     fileContents = fileobj.read()
 
     xbrl_parser = XBRLCorporateFilingParser(symbol='x', xbrl_str=fileContents)
     xbrl_parser.parsedDataFrame.to_csv(r"..\output\xbrl_data.csv", index=False)
-    value = xbrl_parser.get('operating_revenue', 'OneD')
+    value = xbrl_parser.get('EBIT', 'FourD')
     print('Value: ', value)
 
     fileobj.close()
