3d0269ea799f60a007026c67bf2a30923e95a7bc
isahanabhat <isahanabhat@gmail.com>
2023-12-17 12:23:49 +0530

    further changes

--- code/xbrl_parser.py
+++ code/xbrl_parser.py
@@ -47,19 +47,25 @@ class XBRLCorporateFilingParser:
             print('Result type: ', self.resultType)
             print("attribute_df: ")
             print(self.attribute_df)
+            print(self.attribute_df.result_type)
 
         if self.verbosity == 2:
             print(count)
             for i in rowlist:
                 print(i)
 
-    def calcXbrl(self, formula, context):
+    def calcXbrl_recursion(self, attribute, context):
+        formula = self.attribute_df.loc[(self.attribute_df.attribute == attribute) &
+                                        (self.attributeMap['result_type'].str.contains(self.resultType, na=False))] \
+            .value_expr.reset_index(drop=True)[0]
         pattern = "[\[\]]"
         formulaList = re.split(pattern, formula)
         formulaString = ""
-        operands = self.attribute_df["attribute"].tolist()
+
+        operands = dict(zip(self.attribute_df.attribute, self.attribute_df.attribute_type))
+
         for val in formulaList:
-            if val in operands:
+            if val in list(operands.keys()) and operands[val] == 'raw_xbrl':
                 actualAttribute = self.attribute_df.loc[(self.attribute_df.attribute == val) &
                                                         (self.attribute_df['result_type'].str.contains(self.resultType,
                                                                                                        na=False))] \
@@ -68,11 +74,15 @@ class XBRLCorporateFilingParser:
                 numVal = self.parsedDataFrame.loc[(self.parsedDataFrame.Tags == actualAttribute) &
                                                   (self.parsedDataFrame.contextRef == context)].Value.reset_index(
                     drop=True)[0]
+
                 formulaString += numVal
+            elif val in list(operands.keys()) and operands[val] == 'calc_xbrl':
+                tempVal = str(self.calcXbrl_recursion(val, context))
+                formulaString += tempVal
             else:
                 formulaString += val
-
         # Last step to check for any dangerous command
+        print("evaluating...", formulaString)
         assert 'os' not in formulaString and 'system' not in formulaString, \
             'Aborting!!! Dangerous command: %s' % formulaString
         return eval(formulaString)
@@ -87,9 +97,8 @@ class XBRLCorporateFilingParser:
 
         if x.attribute_type[0] == 'raw_xbrl':
             attributeName = self.attribute_df.loc[(self.attribute_df.attribute == attribute) &
-                                                  (self.attribute_df.result_type == self.resultType)] \
+                                                  (self.attribute_df['result_type'].str.contains(self.resultType, na=False))] \
                 .value_expr.reset_index(drop=True)
-
             data = self.parsedDataFrame.loc[(self.parsedDataFrame.Tags == attributeName[0]) &
                                             (self.parsedDataFrame.contextRef == context)].reset_index(drop=True)
             if self.verbosity >= 1:
@@ -99,24 +108,29 @@ class XBRLCorporateFilingParser:
                 print(data)
             resultValue = data.Value[0]
         elif x.attribute_type[0] == 'calc_xbrl':
-            resultValue = self.calcXbrl(x.value_expr[0], context)
-
+            resultValue = self.calcXbrl_recursion(x.attribute[0], context)
+        else:
+            return float('nan')
 
         if x.value_type[0] == 'string':
             return str(resultValue)
         elif x.value_type[0] == 'float':
             return (float(resultValue)) / self.currency_unit
+        elif x.value_type[0] == 'ratioe':
+            return float(resultValue)
+        elif x.value_type[0] == 'int':
+            return int(float(resultValue))
 
 
 if __name__ == '__main__':
-    file_bank = r'../data/BANKING_ICICIBANK.xml'
-    file_default = r'../data/justSample.xml'
+    file_bank = r'../data/BANKING_97822_965308_21102023041426.xml'
+    file_default = r'../data/INDAS_97985_968429_26102023083837.xml'
     fileobj = open(file_bank, 'r')
     fileContents = fileobj.read()
 
     xbrl_parser = XBRLCorporateFilingParser(symbol='x', xbrl_str=fileContents)
     xbrl_parser.parsedDataFrame.to_csv(r"..\output\xbrl_data.csv", index=False)
-    value = xbrl_parser.get('EBIT', 'FourD')
+    value = xbrl_parser.get('face_value_of_share', 'OneD')
     print('Value: ', value)
 
     fileobj.close()
